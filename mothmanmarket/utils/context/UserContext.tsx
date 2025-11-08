3dd12b12d159fa67ec70640be796c9958522c640
Dylan Colburn <129214826+Dcolburn6533@users.noreply.github.com>
2025-11-08 12:01:04 -0500

    Add Dispatch and SetStateAction to UserContext imports

--- mothmanmarket/utils/context/UserContext.tsx
+++ mothmanmarket/utils/context/UserContext.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { createContext, useState, useContext, ReactNode } from "react";
+import { createContext, useState, useContext, ReactNode, Dispatch, SetStateAction } from "react";
 import { createClient } from '@supabase/supabase-js'
 
 interface UserContextType {
